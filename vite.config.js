e6ddc92b2cfabfabf9479138fa5125418119f6df
Tristan Khuong <87681993+triek@users.noreply.github.com>
2025-12-16 06:38:00 +0000

    init tailwind and devtools

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools(), tailwind()],
   base: '/daer/',
   server: {
     proxy: {
